c65d8e8ad4424fcc360f6c620e4e6e2d03551025
EPICZEUS <sbepiczeus@aol.com>
2017-07-22 18:25:22 -0400

    Remove vm from eval

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const Command = require("../command.js");
-const vm = require("vm");
 const {inspect} = require("util");
 
 function clean(str, reg) {
@@ -27,7 +26,7 @@ class EvalCommand extends Command {
     }
 
     async run(message, args) {
-        const embed = new Discord.RichEmbed();
+        const client = this.client, embed = new Discord.RichEmbed();
         const code = args.join(" ");
         const tokenReg = new RegExp(`${this.client.token}|${this.client.token.split("").reverse().join("")}`.replace(/\./g, "\\."), "g");
 
@@ -35,12 +34,10 @@ class EvalCommand extends Command {
 
         console.log(code);
 
-        const script = new vm.Script(code);
-
         const start = process.hrtime();
 
         try {
-            let done = await script.runInNewContext({client:this.client, message, require}, {timeout:5000});
+            let done = await eval(code);
             const hrDiff = process.hrtime(start);
             let end = (hrDiff[0] > 0 ? (hrDiff[0] * 1000000000) : 0) + hrDiff[1];
 
@@ -60,7 +57,7 @@ class EvalCommand extends Command {
                 .setFooter(`Runtime: ${end.toFixed(3)}${ending}`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
                 .setColor(24120);
 
-            return (this.client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))(`**INPUT:** \`${code}\``, {embed});
+            return (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))(`**INPUT:** \`${code}\``, {embed});
         } catch (err) {
             const hrDiff = process.hrtime(start);
             let end = (hrDiff[0] > 0 ? (hrDiff[0] * 1000000000) : 0) + hrDiff[1];
@@ -77,7 +74,7 @@ class EvalCommand extends Command {
                 .setFooter(`Runtime: ${end.toFixed(3)}${ending}`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
                 .setColor(13379110);
 
-            (this.client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))(`**INPUT:** \`${code}\``, {embed}).catch(console.error);
+            (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))(`**INPUT:** \`${code}\``, {embed}).catch(console.error);
         }
     }
 }
