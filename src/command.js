94db150a1cb6ce9ed4196c5628da82acd5683e72
EPICZEUS <sbepiczeus@aol.com>
2017-08-21 14:13:08 -0400

    Improve command custom inspect

--- src/command.js
+++ src/command.js
@@ -138,7 +138,7 @@ class Command {
         return depth > 0 ? `${this.constructor.name} {
     Name: ${this.name},
     Type: ${this.type},
-    Aliases: [ ${this.aliases ? this.aliases.length : 0} ]
+    Aliases: [ ${this.aliases && this.aliases.length ? this.aliases.join(", ") : "<empty>"} ]
 }` : "[Object]";
     }
 
