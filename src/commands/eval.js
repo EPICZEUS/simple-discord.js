701c235d3f3578e3feab105e1418fb6f71fd8a20
EPICZEUS <sbepiczeus@aol.com>
2017-08-25 15:55:18 -0400

    Attempted code improvement in eval.js

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -26,7 +26,7 @@ class Eval extends Command {
     clean(str) {
         const reg = new RegExp(`${this.client.token}|${this.client.token.split("").reverse().join("")}`, "g");
 
-        return typeof str === "string" ? str.replace(/[`@]/g, "$&\u200b").replace(reg, "[SECRET]") : str;
+        return typeof str === "string" ? str.replace(/[`@]/g, "$&\u200b").replace(reg, "[SECRET]").replace(new RegExp(process.cwd(), "g"), ".") : str;
     }
 
     async run(message, args) {
@@ -35,63 +35,51 @@ class Eval extends Command {
 
         client.utils.log(code);
 
+        let evaled, suffix;
         const start = process.hrtime();
 
         try {
-            let done = await eval(code);
-            const hrDiff = process.hrtime(start);
-            let end = (hrDiff[0] > 0 ? (hrDiff[0] * 1000000000) : 0) + hrDiff[1];
-
-            let ending = this.endings[0], i = 0;
-            
-            while (this.endings[++i] && end > 1000) {
-                end /= 1000;
-                ending = this.endings[i];
-            }
-
-            client.utils.log(done);
-
-            if (typeof done !== "string") done = inspect(done);
-
-            let suffix;
-
-            if (done.length > 1800) {
-                message = await (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))("Uploading to gist, this may take a moment...");
-
-                const {id} = await post("https://api.github.com/gists").send({
-                    public: true,
-                    files: {
-                        [`output_${message.author.id}_${moment().format("YYYY_MM_DD")}.js`]: {content:done}
-                    }
-                }).then(res => JSON.parse(res.text));
-
-                suffix = `[Gist created](https://gist.github.com/${id})`;
-            } else {
-                suffix = `\`\`\`js\n${this.clean(done)}\n\`\`\``;
-            }
-
-            embed.setDescription(`**INPUT:** \`\`\`js\n${code}\n\`\`\`\n**OUTPUT:** ${suffix}`)
-                .setFooter(`Runtime: ${end.toFixed(3)}${ending}`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
-                .setColor(24120);
-
-            return (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))({embed});
+            evaled = await eval(code);
+            client.utils.log(evaled);
+            embed.setColor(24120);
+            suffix = `**OUTPUT**: \`\`\`js\n`;
         } catch (err) {
-            const hrDiff = process.hrtime(start);
-            let end = (hrDiff[0] > 0 ? (hrDiff[0] * 1000000000) : 0) + hrDiff[1];
-
-            let ending = this.endings[0], i = 0;
-
-            while (this.endings[++i] && end > 1000) {
-                end /= 1000;
-                ending = this.endings[i];
-            }
+            evaled = err;
             client.utils.error(err);
-            embed.setDescription(`**INPUT:** \`\`\`js\n${code}\n\`\`\`\n<:panicbasket:267397363956580352>**ERROR**<:panicbasket:267397363956580352>\n\`\`\`xl\n${this.clean(err)}\n\`\`\`\n`)
-                .setFooter(`Runtime: ${end.toFixed(3)}${ending}`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
-                .setColor(13379110);
-
-            return (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))({embed});
+            embed.setColor(13379110);
+            suffix = `**ERROR**: \`\`\`xl\n`;
         }
+        const hrDiff = process.hrtime(start);
+        let end = (hrDiff[0] > 0 ? (hrDiff[0] * 1000000000) : 0) + hrDiff[1];
+
+        let ending = this.endings[0], i = 0;
+        
+        while (this.endings[++i] && end > 1000) {
+            end /= 1000;
+            ending = this.endings[i];
+        }
+
+        if (typeof evaled !== "string") evaled = inspect(evaled);
+
+        if (evaled.length > 1800) {
+            message = await (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))("Uploading to gist, this may take a moment...");
+
+            const {id} = await post("https://api.github.com/gists").send({
+                public: true,
+                files: {
+                    [`output_${message.author.id}_${moment().format("YYYY_MM_DD")}.js`]: {content:evaled}
+                }
+            }).then(res => JSON.parse(res.text));
+
+            suffix += `[Gist created](https://gist.github.com/${id})`;
+        } else {
+            suffix += `${this.clean(evaled)}\n\`\`\``;
+        }
+
+        embed.setDescription(`**INPUT:** \`\`\`js\n${code}\n\`\`\`\n${suffix}`)
+            .setFooter(`Runtime: ${end.toFixed(3)}${ending}`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png");
+
+        return (client._selfbot ? message.edit.bind(message) : message.channel.send.bind(message.channel))({embed});
     }
 }
 
