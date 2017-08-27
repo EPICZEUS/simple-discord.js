22392d4737df5a44cd0a883b0addd2e6f61eb5d5
EPICZEUS <sbepiczeus@aol.com>
2017-08-27 14:13:38 -0400

    Add further markdown

--- src/commands/info.js
+++ src/commands/info.js
@@ -25,7 +25,7 @@ class Info extends Command {
         const howTo = cmdFile.use ? cmdFile.use.map(use => use[1] ? `<${use[0]}>` : `[${use[0]}]`).join(" ") : "";
         const use = this.client.prefix ? `${this.client.prefix}${cmdFile.name} ${howTo}` : `${howTo ? `${howTo} ` : ""}${cmdFile.name}${this.client.suffix}`;
 
-        const description = `${cmdFile.description}\n\n**Usage**\n${use}\n\n**Aliases**\n${cmdFile.aliases && cmdFile.aliases.length ? `\`${cmdFile.aliases.join("`, `")}\`` : "None"}`;
+        const description = `\`${cmdFile.description}\`\n\n**Usage**\n${use}\n\n**Aliases**\n${cmdFile.aliases && cmdFile.aliases.length ? `\`${cmdFile.aliases.join("`, `")}\`` : "None"}`;
 
         return (!this.client.user.bot ? message.edit.bind(message) : message.channel.send.bind(message.channel))({embed: {
             title: cmdFile.name.replace(/^./, l => l.toUpperCase()),
